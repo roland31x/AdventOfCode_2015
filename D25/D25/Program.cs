91ec1fda27003b379f4bb308eea6e02b9138ba4e
roland31x <roland31x@gmail.com>
2023-07-13 04:30:17 +0300

    day 25 done

---
+++ D25/D25/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace D25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 2981; // your input
+            int column = 3075; // your input
+            long start = 20151125L;
+            int currentrow = 1;
+            int currentcolumn = 1;
+            int level = 1;
+            long past = start;
+            long current = GetNext(start);
+            while (currentrow != row || currentcolumn != column)
+            {
+                current = GetNext(past);
+                past = current;
+                currentrow--;
+                currentcolumn++;
+                if (currentcolumn > level)
+                {
+                    level++;
+                    currentrow = level;
+                    currentcolumn = 1;                   
+                }
+                
+            }
+            Console.WriteLine("Solution: " + current);
+
+        }
+        static long GetNext(long past)
+        {
+            checked 
+            {
+                return (past * 252533L) % 33554393L; 
+            }
+        }
+    }
+}
